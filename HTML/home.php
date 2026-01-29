5d29224eaa4671bf3ceaaf152e55d87d10edd985
Elsa <er73775@ubt-uni.net>
2026-01-29 21:18:59 +0100

    fixed redirect

--- HTML/home.php
+++ HTML/home.php
@@ -1,12 +1,6 @@
 <?php
-    session_start();
     $pageTitle = 'Home';
     require_once 'header.php';
-
-    if (!isset($_SESSION['user_id'])) {
-       header("Location: login.php");
-       exit;
-    }
 
 ?>
 
