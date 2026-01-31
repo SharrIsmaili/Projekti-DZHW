edb35d9d78210c6f4895cf7bfb2291d3abf3e717
Sharr Ismaili <si72998@ubt-uni.net>
2026-01-31 21:49:33 +0100

    centered the picture

--- HTML/aboutUs.php
+++ HTML/aboutUs.php
@@ -450,8 +450,8 @@
                         our shared mission to make healthcare accessible, effective, and human. Every day, we strive to
                         be a place where care meets hope, science meets empathy, and every patient feels supported and
                         valued.</p>
-                        <img src="../images/about-us/whereWeAre.jpg" alt="Where we are now" style="width: 800px; justify-content:center; display:flex; padding: left 50px;">
                 </div>
+                <img src="../images/about-us/whereWeAre.jpg" alt="Where we are now" style="width: 800px; justify-content:center; display:flex; padding: left 50px;">
             </div>
         </section>
 
